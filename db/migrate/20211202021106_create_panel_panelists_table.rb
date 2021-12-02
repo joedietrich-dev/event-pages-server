731eda08f72b645f5de2b44354cff170f9081dc1
joedietrich-dev <joe@joedietrich.net>
2021-12-01 22:19:18 -0500

    created panel linking table migrations

--- db/migrate/20211202021106_create_panel_panelists_table.rb
+++ db/migrate/20211202021106_create_panel_panelists_table.rb
@@ -1,4 +1,9 @@
 class CreatePanelPanelistsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :panel_panelists do |t|
+      t.references :panel, foreign_key: true
+      t.references :panelist, foreign_key: true
+      t.timestamps
+    end
   end
 end
--- db/migrate/20211202021121_create_panel_sponsors_table.rb
+++ db/migrate/20211202021121_create_panel_sponsors_table.rb
@@ -1,4 +1,9 @@
 class CreatePanelSponsorsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :panel_sponsors do |t|
+      t.references :panel, foreign_key: true
+      t.references :sponsor, foreign_key: true
+      t.timestamps
+    end
   end
 end
