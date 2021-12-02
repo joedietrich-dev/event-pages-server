87a07ca23b3c2b5e323ec9e95260ee3bd13889ce
joedietrich-dev <joe@joedietrich.net>
2021-12-01 21:44:18 -0500

    create sponsor and sponsor level tables

--- db/migrate/20211202020839_create_sponsors_table.rb
+++ db/migrate/20211202020839_create_sponsors_table.rb
@@ -1,4 +1,9 @@
 class CreateSponsorsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :sponsors do |t|
+      t.string :name, null: false
+      t.string :logo_src
+      t.timestamps
+    end
   end
 end
--- db/migrate/20211202020910_create_event_sponsor_levels_table.rb
+++ db/migrate/20211202020910_create_event_sponsor_levels_table.rb
@@ -1,4 +1,9 @@
 class CreateEventSponsorLevelsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :event_sponsor_levels do |t|
+      t.string :name, null: false
+      t.integer :rank, null: false, default: 0
+      t.timestamps
+    end
   end
 end
