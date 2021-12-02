202eeb3ac9166bbbaf09ab729c780ca3906ab17e
joedietrich-dev <joe@joedietrich.net>
2021-12-01 21:36:29 -0500

    create paanelists and events tables

--- db/migrate/20211202020811_create_events_table.rb
+++ db/migrate/20211202020811_create_events_table.rb
@@ -1,7 +1,15 @@
 class CreateEventsTable < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-      t.
+      t.string :title, null: false
+      t.string :description
+      t.string :short_description
+      t.string :location
+      t.string :hero_src
+      t.string :register_link
+      t.string :view_link
+      t.datetime :date
+      t.timestamps
     end
   end
 end
--- db/migrate/20211202020823_create_panelists_table.rb
+++ db/migrate/20211202020823_create_panelists_table.rb
@@ -1,4 +1,13 @@
 class CreatePanelistsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :panelists do |t|
+      t.string :name, null: false
+      t.string :title
+      t.string :company
+      t.string :bio
+      t.string :headshot_src
+      t.string :is_moderator, null: false, default: false
+      t.timestamps
+    end
   end
 end
