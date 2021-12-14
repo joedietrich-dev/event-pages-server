7a8dabd2134774d25814d91179602dc5fb1ae28d
joedietrich-dev <joe@joedietrich.net>
2021-12-13 22:50:41 -0500

    Add routes for hosts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,11 +33,41 @@ class ApplicationController < Sinatra::Base
     Honoree.all.to_json
   end
 
+  post "/honorees" do
+    honoree = Honoree.create(
+      name: params[:name], 
+      descriptor: params[:descriptor], 
+      honor: params[:honor], 
+      bio: params[:bio],
+      img_src: params[:img_src],
+      event_id: params[:event_id] || nil
+    )
+    honoree.to_json
+  end
+
   get "/honorees/:id" do
     Honoree.find(params[:id]).to_json(
       include: [event: {only: [:id, :title]}],
       except: :event_id
     )
+  end
+
+  patch "/honorees/:id" do
+    honoree = Honoree.find(params[:id])
+    honoree.update(
+      name: params[:name], 
+      descriptor: params[:descriptor], 
+      honor: params[:honor], 
+      bio: params[:bio],
+      img_src: params[:img_src],
+      event_id: params[:event_id] ||= honoree[:event_id]
+    )
+    honoree.to_json
+  end
+
+  delete "/honorees/:id" do
+    honoree = Honoree.find(params[:id])
+    honoree.destroy
   end
 
   get "/hosts" do
