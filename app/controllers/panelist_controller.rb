4595699388e58a278fed48d4ad577d10c47736fa
joedietrich-dev <joe@joedietrich.net>
2021-12-14 20:14:56 -0500

    Create Panelist Controller

--- app/controllers/panelist_controller.rb
+++ app/controllers/panelist_controller.rb
@@ -5,6 +5,17 @@ class PanelistController < Sinatra::Base
     Panelist.all.to_json
   end
 
+  post "/panelists" do
+    panelist = Panelist.create(
+      name: params[:name],
+      title: params[:title],
+      company: params[:company],
+      bio: params[:bio],
+      headshot_src: params[:headshot_src]
+    )
+    panelist.to_json
+  end
+
   get "/panelists/:id" do
     panelist = Panelist.includes(:panel_panelists, panels: [:event]).find(params[:id])
     res = {
@@ -26,5 +37,22 @@ class PanelistController < Sinatra::Base
       updated_at: panelist.updated_at
      }
     res.to_json
+  end
+
+  patch "/panelists/:id" do
+    panelist = Panelist.find(params[:id])
+    panelist.update(
+      name: params[:name],
+      title: params[:title],
+      company: params[:company],
+      bio: params[:bio],
+      headshot_src: params[:headshot_src]
+    )
+    panelist.to_json
+  end
+
+  delete "/panelists/:id" do
+    panelist = Panelist.find(params[:id])
+    panelist.destroy
   end
 end
