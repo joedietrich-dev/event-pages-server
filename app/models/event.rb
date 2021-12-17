5c93733bf6db7aba02bd722d2881f03dc195a982
joedietrich-dev <joe@joedietrich.net>
2021-12-16 20:45:06 -0500

    Add appropriate dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ActiveRecord::Base
-  has_many :panels
+  has_many :panels, dependent: :destroy
   has_many :panel_panelists, through: :panels
-  has_many :hosts
-  has_many :honorees
-  has_many :event_sponsors
+  has_many :hosts, dependent: :destroy
+  has_many :honorees, dependent: :destroy
+  has_many :event_sponsors, dependent: :destroy
   has_many :sponsors, through: :event_sponsors
   has_many :event_sponsor_levels, through: :event_sponsors
 
--- app/models/panel.rb
+++ app/models/panel.rb
@@ -1,8 +1,8 @@
 class Panel < ActiveRecord::Base
   belongs_to :event
-  has_many :panel_panelists
+  has_many :panel_panelists, dependent: :destroy
   has_many :panelists, through: :panel_panelists
-  has_many :panel_sponsors
+  has_many :panel_sponsors, dependent: :destroy
   has_many :sponsors, through: :panel_sponsors
   
   validates :title, presence: true
